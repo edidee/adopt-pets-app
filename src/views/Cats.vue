3f58b1911dd428a575d1ea733fa8929c3ac11d71
edidee <etokedidionge@gmail.com>
2021-01-09 21:52:25 +0100

    added data and table

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -1,13 +1,23 @@
 <template>
-    <div>
-        Cats
-    </div>
+  <div>
+      <h1>Cats for Adoption</h1>
+    <b-table striped hover :items="cats"></b-table>
+  </div>
 </template>
 
 <script>
-export default { }
+export default {
+  data () {
+    return {
+      cats: [
+        { Name: 'Henry', Breed: 'Tabby', Gender: 'Male', Age: '19', Color: 'orange/white' },
+        { Name: 'Roger', Breed: 'Tabby', Gender: 'Female', Age: '9', Color: 'black' },
+        { Name: 'Kitkat', Breed: 'Bombay', Gender: 'Female', Age: '6', Color: 'white' },
+        { Name: 'Fish', Breed: 'tuxedo', Gender: 'Male', Age: '20', Color: 'gray' }
+      ]
+    }
+  }
+}
 </script>
 
-<style lang="stylus">
-
-</style>
+<style lang="stylus"></style>
--- src/views/Dogs.vue
+++ src/views/Dogs.vue
@@ -1,5 +1,21 @@
 <template>
     <div>
-        Dogs
+        <h1>Dogs for Adoption</h1>
+       <b-table striped hover :items="dogs"></b-table>
     </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      dogs: [
+        { Name: 'Billy', Breed: 'Asian', Gender: 'Male', Age: '9', Color: 'gray' },
+        { Name: 'Arrow', Breed: 'Golden Retriever', Gender: 'Female', Age: '4', Color: 'black' },
+        { Name: 'Major', Breed: 'Asian', Gender: 'Female', Age: '6', Color: 'white' },
+        { Name: 'Captain', Breed: 'Asian', Gender: 'Male', Age: '7', Color: 'gray' }
+      ]
+    }
+  }
+}
+</script>
