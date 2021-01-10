711603a7f810f7611752d90efa61a5c617cdac55
edidee <etokedidionge@gmail.com>
2021-01-10 15:23:08 +0100

    update pets page

---
+++ src/store/state.js
@@ -0,0 +1,7 @@
+import Cats from '../views/Cats.vue'
+import Dogs from '../views/Dogs.vue'
+
+export default {
+  Cats,
+  Dogs
+}
--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
       <h1>Cats for Adoption</h1>
-    <b-table striped hover :items="cats"></b-table>
+    <b-table striped hover :items="cats">
+        <template #cell(name)="data">
+        <router-link
+         :to="`/pets/${data.index}`">{{ data.value }}</router-link>
+      </template>
+    </b-table>
   </div>
 </template>
 
 <script>
+
 export default {
   data () {
     return {
